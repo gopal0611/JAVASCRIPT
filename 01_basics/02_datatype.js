8b62714525c28938b9b545e3a5d399e339bc7b75
Gopal Prasad <125894253+gopal0611@users.noreply.github.com>
2024-01-02 18:47:44 +0530

    Update 02_datatype.js

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -1,3 +1,28 @@
 "use strict"; // treat all js code as newer version
 
 // alert(3+3) // we are using nodejs, not browser
+
+console.log(3 + 3);
+console.log("Gopal");
+ 
+
+let name = "Gopal" // sting
+let age = 13 // number
+let isLoggedIn = false// boolean
+
+
+// number => 2 to power 3
+// binint
+// string => ""
+// boolean => true / false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+
+// object
+
+
+console.log(typeof age)
+console.log(typeof null)
