b7b9ae56bb508188a8835ebc2221c0bb49084664
Gopal Prasad <125894253+gopal0611@users.noreply.github.com>
2024-01-03 15:00:53 +0530

    Update 04_comparison.js

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -7,6 +7,9 @@
 // console.log("2" > 1);
 // console.log("02" > 1); 
 
+
+
+//================= We should avoide this type of conversions =======================
 console.log( null > 0);  // (1)
 console.log(null == 0);
 console.log(null >= 0); // (3)
@@ -17,3 +20,13 @@ The reason is that an equality check = and comparisons > < >=
 Comparisons convert null to a number, treating it as 0.
 That's why (3) >= 0 is true and (1) null > 0 is false.
 */
+
+
+console.log( undefined == 0);
+console.log( undefined > 0);
+console.log( undefined < 0);
+
+
+// ===========
+
+console.log("2" === 2)
