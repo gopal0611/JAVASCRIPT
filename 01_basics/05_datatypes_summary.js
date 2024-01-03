1ba8a6ce0d10d0d6df9bc1b6ac7446c625844f7e
Gopal Prasad <125894253+gopal0611@users.noreply.github.com>
2024-01-03 21:04:24 +0530

    update

--- 01_basics/05_datatypes_summary.js
+++ 01_basics/05_datatypes_summary.js
@@ -1,39 +1,60 @@
-// primitive 
-
+// primitive
 
 // 7 types : String, Number, Boolean, null, undefine, Symbol, BigInt
 
 // js is dynamic type language
 
-const score = 1002  
-const scoreValue = 65.1 
+const score = 1002;
+const scoreValue = 65.1;
 
-const isLoggedIn = false
-const outSideTemp = null
+const isLoggedIn = false;
+const outSideTemp = null;
 let userEmail;
 
-const id = Symbol('163')
-const anotherId = Symbol('163')
+const id = Symbol("163");
+const anotherId = Symbol("163");
 
 console.log(id === anotherId);
 
-
-const bigNum = 5646546498216841n
+const bigNum = 5646546498216841n;
 
 //  Reference (Non primitive)
 
 // Array, Objects, Functions
 
-
 const heros = ["shaktiman", "maagraj", "doga"];
 
 let myObj = {
-    name: "gopal",
-    age: 22,
-}
+  name: "gopal",
+  age: 22,
+};
 
 const myFunction = function () {
-    console.log("Hello World");
-}
+  console.log("Hello World");
+};
 
-console.log(typeof heros)
+console.log(typeof heros);
+
+// ******************************************************************************
+
+// Stack (Primitive), Heap (Non Primitive)
+
+let myYt = "legendgamer";
+
+let anotherName = myYt;
+anotherName = "Kamlesh ";
+
+console.log(anotherName);
+console.log(myYt);
+
+let userOne = {
+  email: "asdf@gmail.com",
+  upi: "283hsdhak@oksbi",
+};
+
+let userTwo = userOne
+
+userTwo.email = "user1243@kk.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
