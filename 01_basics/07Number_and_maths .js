81407c987c7a4d4b4aaa3d52435c8d36c43e51c4
Gopal Prasad <125894253+gopal0611@users.noreply.github.com>
2024-01-04 21:18:48 +0530

    h,

--- 01_basics/07Number_and_maths .js
+++ 01_basics/07Number_and_maths .js
@@ -3,3 +3,14 @@ const score = 400
 const balance = new Number(100)
 console.log(balance);
 console.log(score); 
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2))
+
+const otherNumber = 375.8434
+console.log(otherNumber.toPrecision(3))
+
+const hund = 1000000000000000
+console.log(hund.toLocaleString('en-In'))
