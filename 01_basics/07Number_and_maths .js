fc42c3efba72578d7468d11ddfb8ba3031b10d07
Gopal Prasad <125894253+gopal0611@users.noreply.github.com>
2024-01-04 21:44:28 +0530

    Update 07Number_and_maths .js

--- 01_basics/07Number_and_maths .js
+++ 01_basics/07Number_and_maths .js
@@ -1,16 +1,39 @@
 const score = 400
 
 const balance = new Number(100)
-console.log(balance);
-console.log(score); 
+// console.log(balance);
+// console.log(score); 
 
-console.log(balance.toString());
-console.log(balance.toString().length);
+// console.log(balance.toString());
+// console.log(balance.toString().length);
 
-console.log(balance.toFixed(2))
+// console.log(balance.toFixed(2))
 
 const otherNumber = 375.8434
-console.log(otherNumber.toPrecision(3))
+// console.log(otherNumber.toPrecision(3))
 
 const hund = 1000000000000000
-console.log(hund.toLocaleString('en-In'))
+// console.log(hund.toLocaleString('en-In'))
+
+
+
+
+// #################### Maths ###################
+
+// console.log(Math)
+// console.log(Math.abs(-5));
+// console.log(Math.round(823.32));
+// console.log(Math.ceil(823.32));
+// console.log(Math.floor(823.32));
+// console.log(Math.max(823, 34, 346, 346, 32));
+// console.log(Math.min(823, 34, 346, 346, 32));
+
+
+console.log(Math.random()); 
+console.log((Math.random()*10) + 1); 
+console.log(Math.floor(Math.random()*10) + 1); 
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
