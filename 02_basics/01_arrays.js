9af27c57bebc2f9ff6b5deff5593848bfbe80908
Gopal Prasad <125894253+gopal0611@users.noreply.github.com>
2024-01-05 20:08:26 +0530

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,48 @@
+// // array
+
+const myArr = [0,1,2,3,4,5,6,7,8,9 ]
+// const myHero = ["shaktiman", "nagraj"]
+
+// const myArr1 = new Array(0,1,2,3,4,5)
+// console.log(myArr[0]);
+// console.log(myArr[1])
+    
+
+//     // Array methods
+
+//     // myArr.push(99)
+//     // myArr.push(98)
+    // myArr.pop()
+
+// // myArr.unshift(55)
+// // myArr.shift()
+
+// // console.log(myArr.includes(8));
+// // console.log(myArr.indexOf(8));
+
+
+// const newArr = myArr.join()
+
+
+
+//     console.log(myArr);
+//     console.log(typeof newArr)
+
+
+
+// slice, splice 
+
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1)
+console.log("B", myArr)
+
+const myn2 = myArr.splice(1,3)
+console.log("C", myArr);
+
+console.log(myn1)
+
+
