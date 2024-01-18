03dfcdc12487d5ab09ece3c57f393317a96810c7
Gopal Prasad <125894253+gopal0611@users.noreply.github.com>
2024-01-18 16:41:53 +0530

    Update 04_objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -18,3 +18,10 @@ const regularUser = {
 }
 
 // console.log(regularUser.fullname.userfullname.firstname);
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1, obj2}
+// console.log(obj3); 
