bff6e88f12dd901d18102238a56b474ffc72b238
Gopal Prasad <125894253+gopal0611@users.noreply.github.com>
2024-01-18 19:44:08 +0530

    Update 04_objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -22,6 +22,8 @@ const regularUser = {
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
+const obj3 = {5: "a", 6: "b"}
 
 // const obj3 = {obj1, obj2}
-// console.log(obj3); 
+const obj4 = Object.assign({}, obj1, obj2, obj3)
+console.log(obj4); 
