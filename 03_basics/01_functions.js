7a7e110fa0b9cec7e6a04653652e36b3d10763b8
Gopal Prasad <125894253+gopal0611@users.noreply.github.com>
2024-01-19 20:44:34 +0530

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -30,3 +30,35 @@ function loginUserMessage(username = "sam") {
 }
 // console.log(loginUserMessage("Gopal"));
 // console.log(loginUserMessage("Gopal"));
+
+
+
+function calculateCartPrice(...num1){
+  return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+  username: "Gopal",
+  price: 199
+}
+
+function handleObject(anyobject){
+console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+  username: "sam",
+  price: 99
+})
+
+const myNewArray = [200, 300, 400, 500]
+
+function returnSecondValue(getArray){
+  return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 600, 800,1000]));
