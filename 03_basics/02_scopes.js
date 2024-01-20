f3b35419826a05daca5b5814316cb26e91133b71
Gopal Prasad <125894253+gopal0611@users.noreply.github.com>
2024-01-20 15:01:21 +0530

    scops

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,7 +1,65 @@
-let a = 10
-const b = 20
-var c = 30
+// var c = 300
+let a = 300;
 
-console.log(a)
-console.log(b);
-console.log(c);
+if (true) {
+  let a = 10;
+  const b = 20;
+//   console.log("Inner: ", a);
+}
+
+// console.log(a)
+// console.log(b);
+// console.log(c);
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+
+// console.log("Outer: ", a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const userName = "Gopal"
+
+    function two(){
+        const website = "Youtube"
+        console.log(userName);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+
+if(true) {
+    const username = "Gopal"
+    if (username === "Gopal") {
+        const website = " youtube"
+        // console.log(username + website );
+    }
+// console.log(website);
+
+}
+
+// console.log(username)
+
+
+// ++++++++++++++++++++++++++++++++ Intresting +++++++++++++++++++++++++++++
+
+console.log(addone(5))
+function addone(num){
+ return num + 1
+}
+
+
+const addTwo = function(num){
+    return num + 2
+
+}
+addTwo(5)
